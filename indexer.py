498857bf51ae59a2835b57f5fd9f45f4ad072fbd
Scytrin dai Kinthra <scytrin@gmail.com>
2013-01-10 13:19:40 -0800

    more yay!

--- indexer.py
+++ indexer.py
@@ -3,13 +3,15 @@ import ConfigParser
 from cStringIO import StringIO
 import logging
 import nntplib
+from multiprocessing import BoundedSemaphore, Process, Queue
+import random
 import signal
 import sqlite3
-
-
+import sys
+import time
 
 sqlite3.enable_callback_tracebacks(True)
-logging.basicConfig(format="%(levelname)s (%(threadName)s) %(filename)s:%(lineno)d %(message)s")
+logging.basicConfig(format="%(levelname)s (%(processName)s:%(threadName)s) %(filename)s:%(lineno)d %(message)s")
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 
@@ -35,17 +37,30 @@ class NNTPCache:
                       'UNIQUE(group_name, number) ON CONFLICT REPLACE)')
       self.db.commit()
 
+  def __enter__(self):
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.db.close()
+
   def add_group(self, group_name):
     LOG.debug((group_name,))
     self.add_groups([(group_name,)])
 
   def add_groups(self, group_names):
     # [ (group_name,), ... ]
-    LOG.debug((len(articles), articles))
+    #LOG.debug((len(group_names), group_names))
     statement = 'INSERT OR REPLACE INTO groups VALUES (?, ?)'
+    arguments = [ (False, g[0]) for g in group_names]
     db_cursor = self.db.cursor()
-    db_cursor.executemany(statement, [ (False, g) for g in group_names])
-    self.db.commit()
+    while True:
+      try:
+        db_cursor.executemany(statement, arguments)
+        self.db.commit()
+        break
+      except sqlite3.OperationalError as err:
+        LOG.error(err)
+        time.sleep(random.randint(1,3))
 
   def add_article(self, subject, group_name, message_id, number):
     LOG.debug((subject, group_name, message_id, number))
@@ -54,12 +69,20 @@ class NNTPCache:
   def add_articles(self, articles):
     # [ (subject, message_id, group, number), ... ]
     LOG.debug((len(articles), articles[0]))
-    db_cursor = self.db.cursor()
     statement1 = 'INSERT OR REPLACE INTO articles VALUES (?, ?)'
+    arguments1 = [ a[:2] for a in articles ]
     statement2 = 'INSERT OR REPLACE INTO group_articles VALUES (?, ?, ?)'
-    db_cursor.executemany(statement1, [ a[:2] for a in articles ])
-    db_cursor.executemany(statement2, [ a[1:] for a in articles ])
-    self.db.commit()
+    arguments2 = [ a[1:] for a in articles ]
+    db_cursor = self.db.cursor()
+    while True:
+      try:
+        db_cursor.executemany(statement1, arguments1)
+        db_cursor.executemany(statement2, arguments2)
+        self.db.commit()
+        break
+      except sqlite3.OperationalError as err:
+        LOG.error(err)
+        time.sleep(random.randint(1,3))
 
   def get_article(self, message_id):
     LOG.debug((message_id,))
@@ -86,9 +109,16 @@ class NNTPCache:
   def set_watched(self, group_name, watch=True):
     LOG.debug((group_name, watch))
     statement = 'UPDATE groups SET watch = ? WHERE group_name = ?'
+    arguments = ( watch, group_name )
     db_cursor = self.db.cursor()
-    db_cursor.execute(statement, ( watch, group_name ))
-    self.db.commit()
+    while True:
+      try:
+        db_cursor.execute(statement, arguments)
+        self.db.commit()
+        break
+      except sqlite3.OperationalError as err:
+        LOG.error(err)
+        time.sleep(random.randint(1,3))
 
   def get_watched(self):
     statement = 'SELECT group_name FROM groups WHERE watch'
@@ -98,22 +128,122 @@ class NNTPCache:
 
 
 
-class NNTP(nntplib.NNTP):
+class NNTPWorker(nntplib.NNTP):
+  def __init__(self, semaphore, *args, **kw):
+    self.semaphore = semaphore
+    self.semaphore.acquire()
+    nntplib.NNTP.__init__(self, *args, **kw)
+
   def __enter__(self):
     return self
+
   def __exit__(self, type, value, traceback):
     self.quit()
+
+  def quit(self):
+    nntplib.NNTP.quit(self)
+    self.semaphore.release()
+
+
+
+class Indexer:
+  def __init__(self, config_file='defaults.cfg'):
+    self.config = ConfigParser.SafeConfigParser()
+    self.config.readfp(open(config_file))
+
+    self.actions = BoundedSemaphore(20)
+    self.nntp_c = NNTPCache(self.config, True)
+    max_connections = self.config.getint('indexer', 'max_connections')
+    self.nntp_semaphore = BoundedSemaphore(max_connections)
+
+  @property
+  def nntp_connection(self):
+    worker = NNTPWorker(
+        self.nntp_semaphore,
+        self.config.get('indexer', 'host'),
+        self.config.get('indexer', 'port'),
+        self.config.get('indexer', 'username'),
+        self.config.get('indexer', 'password'),
+        True)
+    #worker.set_debuglevel(1)
+    return worker
+
+  @property
+  def cache_connection(self):
+    return NNTPCache(self.config)
+
+  def update_watched(self):
+    with self.cache_connection as cache:
+      for watched in cache.get_watched():
+        self.update_group(watched)
+
+  def update_group(self, group):
+    last = 0
+    last_read = self.nntp_c.get_last_read(group)
+    span = self.config.getint('indexer', 'xover_range')
+    with self.nntp_connection as nntp:
+      resp, count, first, last, group_name = nntp.group(group)
+      last_read = max(int(first), last_read)
+      last = int(last)
+
+    LOG.debug('group: %s %d %d', group, last, last_read)
+
+    if last > last_read:
+      for start in xrange(last_read, last, span):
+        end = min(last, start + span)
+        self.start_action(self.fetch_group_articles, (group, start, end),
+            'get_group_articles %s %s %s' % (group, start, end))
+
+
+  # Actions, may be in a process or thread! Beware DB locking!
+  def start_action(self, func, args, name=None):
+    with self.actions:
+      proc = Process(name=name, target=func, args=args)
+      proc.daemon = True
+    proc.start() # possible deadlock if in the with statement
+    return proc
+
+  def build_group_list(self):
+    try:
+      with self.actions:
+        with self.nntp_connection as nntp:
+          resp, group_list = nntp.list()
+
+        with self.cache_connection as cache:
+          # count, first, last, name
+          cache.add_groups([ (g[0],) for g in group_list ])
+    except KeyboardInterrupt:
+      #LOG.critical('KB INTERRUPT')
+      return
+
+  def fetch_group_articles(self, group, start, end):
+    try:
+      with self.actions:
+        with self.nntp_connection as nntp:
+          LOG.info("fetching %s: %s - %s ...", group, start, end)
+          group_resp = nntp.group(group)
+          resp, articles = nntp.xover(str(start), str(end))
+
+        with self.cache_connection as cache:
+          LOG.info("storing %s: %s - %s ...", group, start, end)
+          # a_no, subject, poster, when, a_id, refs, sz, li
+          cache.add_articles([ (a[1], a[4], group, a[0]) for a in articles ])
+    except KeyboardInterrupt:
+      #LOG.critical('KB INTERRUPT')
+      return
 
 
 
 class HTTPServer(BaseHTTPServer.HTTPServer):
   class IndexerView(BaseHTTPServer.BaseHTTPRequestHandler):
     BASE_PAGE = '<!DOCTYPE>\n<html><head>\n\t<title>\'dex</title>\n</head><body>%s</body></html>'
-    ARTICLE = '<div>%s</div>'
+    ARTICLE = '<div>%s</div>\n'
+
     def header_index(self):
-      cache = self.server.cache
+      with self.server.indexer.cache_connection as cache:
+        articles = cache.get_articles(limit=100)
       buf = StringIO()
-      for article in cache.get_articles(limit=100):
+      for article in articles:
         buf.write(self.ARTICLE % article[0])
       return self.BASE_PAGE % buf.getvalue()
 
@@ -124,91 +254,18 @@ class HTTPServer(BaseHTTPServer.HTTPServer):
       self.wfile.write(self.header_index())
       return
 
-  def __init__(self, config):
-    self.config = config
+  def __init__(self, config_file='defaults.cfg'):
+    self.indexer = Indexer(config_file)
+    self.config = self.indexer.config
     host = self.config.get('server', 'host')
     port = self.config.getint('server', 'port')
     BaseHTTPServer.HTTPServer.__init__(self, (host, port), self.IndexerView)
-
-  @property
-  def cache(self):
-    return NNTPCache(self.config)
 
   def start(self):
     try: self.serve_forever()
     except KeyboardInterrupt: self.socket.close()
 
 
-
-class Indexer:
-  def __init__(self, config_file='defaults.cfg'):
-    self.config = ConfigParser.SafeConfigParser()
-    self.config.readfp(open(config_file))
-    self.max_connections = self.config.getint('indexer', 'max_connections')
-    self.cache = NNTPCache(self.config, True)
-
-  def _new_connection(self):
-    nntp = NNTP(
-        self.config.get('indexer', 'host'),
-        self.config.get('indexer', 'port'),
-        self.config.get('indexer', 'username'),
-        self.config.get('indexer', 'password'),
-        True)
-    nntp.set_debuglevel(1)
-    return nntp
-
-  def _new_http_server(self):
-    http = HTTPServer(self.config)
-    return http
-
-  @property
-  def http(self):
-    return self._new_http_server()
-
-  @property
-  def nntp(self):
-    return self._new_connection()
-
-  def build_group_list(self):
-    with self.nntp as nntp:
-      resp, group_list = nntp.list()
-    self.cache.add_groups([ (g[0],) for g in group_list ])
-
-  def update_watched(self):
-    apply(self.update_group, self.cache.get_watched())
-
-  def update_group(self, group):
-    last = 0
-    last_read = self.cache.get_last_read(group)
-    span = self.config.getint('indexer', 'xover_range')
-    with self.nntp as nntp:
-      resp, count, first, last, group_name = nntp.group(group)
-      last_read = max(int(first), last_read)
-      last = int(last)
-
-    LOG.debug('group: %s %d %d', group, last, last_read)
-
-    if last > last_read:
-      for start in xrange(last_read, last, span):
-        end = min(last, start + span)
-        articles = self.get_group_articles(group, start, end)
-        LOG.debug('articles %s %s', group, len(articles))
-        # a_no, subject, poster, when, a_id, refs, sz, li
-        self.cache.add_articles([ (a[1], a[4], group, a[0]) for a in articles ])
-
-  def get_group_articles(self, group, start, end):
-    LOG.info("fetching %s: %s - %s ...", group, start, end)
-    with self.nntp as nntp:
-      group_resp = nntp.group(group)
-      resp, articles = nntp.xover(str(start), str(end))
-      return articles
-
-def init_worker():
-  signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-
 def main():
-  indexer = Indexer('defaults.cfg')
-  indexer.http.start()
-  #indexer.build_group_list()
-  #indexer.update_watched()
+  app = HTTPServer()
+  app.start()
--- run.py
+++ run.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python2.7
 
-import ConfigParser
-import logging
-import multiprocessing
-from multiprocessing import Pool, Process
-from nntplib import NNTP
-import signal
-import StringIO
-import sqlite3
-from Queue import Queue
-import thread
-import threading
-import time
-
-from indexer import Indexer
-
-logging.basicConfig(format="%(levelname)s (%(threadName)s) %(filename)s:%(lineno)d %(message)s")
-LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.DEBUG)
+import indexer
 
 def main():
-  indexer = Indexer('defaults.cfg')
-  #indexer.build_group_list()
-  indexer.update_watched()
+  app = indexer.Indexer('defaults.cfg')
+  app.build_group_list()
+  with app.cache_connection as cache:
+    cache.set_watched('alt.binaries.tv', True)
+    cache.set_watched('alt.binaries.hdtv', True)
+  app.update_watched()
 
 if __name__ == "__main__":
   main()
